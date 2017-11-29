6d48833b1a19979658636945e797aedbef954b00
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2017-11-29 11:03:34 -0300

    Add SemaforoInterface, move Makefile

---
+++ src/SemaforoInterface.java
@@ -0,0 +1,30 @@
+/*
+ * Tarea 2 Sistemas Distribuidos 2017-2
+ *
+ * Primera versión de la tarea para la implementación de semaforos distribuidos,
+ * utilizando el algoritmo Suzuki-Kasami, en conjunto con Java RMI.
+ * Desarollada por Andrés Huerta (@MeatBoyUSM) y Felipe Vega (@Umauro)
+ *
+ * 28/11/2017
+ *
+ * INSERTAR LO DEL COPYWEA ACÁ
+ */
+
+ import java.rmi.Remote;
+ import java.rmi.RemoteException;
+
+ /*
+  * Interfaz de semáforos que utilizarán Java RMI
+  *
+  * v0.0.1
+  * 28 nov 2017
+  * Andrés Huerta
+  * Felipe Vega
+  */
+
+public interface SemaforoInterface extends Remote{
+    void request(int id, int seq) throws RemoteException;
+    void waitToken() throws RemoteException;
+    Token takeToken(Token token) throws RemoteException;
+    void kill() throws RemoteException;
+}
