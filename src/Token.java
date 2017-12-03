678e5d29b1f55e96ccee4b3666bcbf2b1b94bb96
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2017-12-03 12:24:08 -0300

    Agregar Main en Token

--- src/Token.java
+++ src/Token.java
@@ -61,4 +61,24 @@ public class Token extends UnicastRemoteObject implements InterfaceRMI{
         System.exit(0);
     }
 
+    public static void main(String[] args){
+       if(System.getSecurityManager() == null){
+           System.setSecurityManager(new RMISecurityManager());
+       }
+
+       try{
+           InterfaceRMI inter = new Token();
+           Naming.rebind("rmi://localhost:" + args[0] + "/SK", inter);
+       }
+       catch (RemoteException e){
+           System.err.println("Error: " + e.toString());
+           System.exit(1);
+       }
+       catch (Exception e){
+           System.err.println("Excepcion: ");
+           e.printStackTrace();
+           System.exit(1);
+       }
+   }
+
 }
